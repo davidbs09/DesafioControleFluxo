9313ae41c24d68b8ca7d9053e450c709d51f18cd
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-06-18 01:22:31 -0300

    update: project finished

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,33 @@
-public class Contador{
+import java.util.Scanner;
+
+public class Contador {
     public static void main(String[] args) {
-        
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+        terminal.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        } else if (parametroUm == parametroDois) {
+            throw new ParametrosInvalidosException("Os parâmetros não podem ser iguais");
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
